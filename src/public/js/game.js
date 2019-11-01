ef26730f58e321d97aa523f9e07f36864d591404
Lucas Guerra <lucasguerratee@gmail.com>
2019-11-01 13:02:32 -0300

    adding the player

--- src/public/js/game.js
+++ src/public/js/game.js
@@ -11,7 +11,7 @@ var BootScene = new Phaser.Class({
     preload: function (){
         this.load.image('tiles', 'public/assets/map/spritesheet.png');
         this.load.tilemapTiledJSON('map', 'public/assets/map/map.json');
-        this.load.spritesheet('player', 'public/assets/RPG_assets.png', { frameWidth: 16, frameHeight: 16 });
+        this.load.spritesheet('player', 'public/assets/player.png', { frameWidth: 16, frameHeight: 16 });
     },
  
     create: function (){
@@ -33,13 +33,113 @@ var WorldScene = new Phaser.Class({
         
     },
     create: function (){
+
+        // Creating the world / Cenario e Objetos
+
         var map = this.make.tilemap({ key: 'map' });
         var tiles = map.addTilesetImage('spritesheet', 'tiles');
 	    var grass = map.createStaticLayer('Grass', tiles, 0, 0);
         var obstacles = map.createStaticLayer('Obstacles', tiles, 0, 0);
         
         obstacles.setCollisionByExclusion([-1]);
+
+        // Creating the player / Jogador
+
+        this.player = this.physics.add.sprite(50, 100, 'player', 6);
         
+        this.physics.world.bounds.width = map.widthInPixels;
+        this.physics.world.bounds.height = map.heightInPixels;
+        this.player.setCollideWorldBounds(true);
+        
+        this.physics.add.collider(this.player, obstacles);
+        
+        // Setting up the controls / Controles
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        // Controls and Animations / Controles e Animacao
+
+        this.anims.create({
+            key: 'down',
+            frames: this.anims.generateFrameNumbers('player', { frames: [ 0, 1, 0, 2 ] }),
+            frameRate: 10,
+            repeat: -1
+        });  
+
+        this.anims.create({
+            key: 'right',
+            frames: this.anims.generateFrameNumbers('player', { frames: [3, 4, 3, 5] }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'left',
+            frames: this.anims.generateFrameNumbers('player', { frames: [8, 7, 8, 6]}),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key: 'up',
+            frames: this.anims.generateFrameNumbers('player', { frames: [9, 10, 9, 11]}),
+            frameRate: 10,
+            repeat: -1
+        });       
+
+        // Camera 
+	
+        this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+        this.cameras.main.startFollow(this.player);
+        this.cameras.main.roundPixels = true;
+
+    },
+
+    update: function (time, delta)
+    {
+    this.player.body.setVelocity(0);
+
+        // Horizontal 
+        if (this.cursors.left.isDown)
+        {
+            this.player.body.setVelocityX(-80);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.body.setVelocityX(80);
+        }
+
+        // Vertical 
+        if (this.cursors.up.isDown)
+        {
+            this.player.body.setVelocityY(-80);
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.player.body.setVelocityY(80);
+        }    
+
+        // Animating / Animando
+        if (this.cursors.left.isDown)
+        {
+            this.player.anims.play('left', true);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.anims.play('right', true);
+        }
+        else if (this.cursors.up.isDown)
+        {
+            this.player.anims.play('up', true);
+        }
+        else if (this.cursors.down.isDown)
+        {
+            this.player.anims.play('down', true);
+        }
+        else
+        {
+            this.player.anims.stop();
+        }
     }
 });
  
