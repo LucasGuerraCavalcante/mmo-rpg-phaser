584b68527383de8eb9fdd63ace54e702de2bb70f
Felipe Maia <felipe.maia@sempreceub.com>
2019-11-28 08:43:00 -0300

    heroku

--- server.js
+++ server.js
@@ -7,13 +7,7 @@ app.use(express.static(__dirname + '/src'));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-
-dom.window.gameLoaded = () => {
-  let port = process.env.PORT;
-  if (port == null || port == "") {
-    port = 8082;
-  }
-  server.listen(port, function () {
-    console.log(`Enjoy =D port:${server.address().port}`);
-  });
-};
+ 
+server.listen(1337, function () {
+  console.log(`Listening on ${server.address().port}`);
+});
